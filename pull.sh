70af1a48bdafdbfe5389a852e8708ef529b39703
ryo.nagata <ryo.nagata@chillnn.com>
2021-09-20 16:51:57 +0900

    update

---
+++ pull.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+if [ $branch != 'prd' -a $branch != 'stg' -a $branch != 'dev' ]; then
+    echo ""
+    echo ""
+    echo "\033[43;31m##############################################\033[0m"
+    echo "\033[43;31m## FetchするABRのbranch名を選んでください。 ##\033[0m"
+    echo "\033[43;31m##############################################\033[0m"
+    echo ""
+
+    select VAR in $branch 'dev' exit
+    do
+        branch=$VAR
+        break
+    done
+fi
+
+if [ $branch != 'exit' ]; then
+    echo ""
+    echo ""
+    echo "\033[43;31m$branch ABR HOTELをFetchします。\033[0m"
+    yarn add git+ssh://git@github.com:ryoNagata001/chillnn-training-abr.git#${branch}
+fi
+
