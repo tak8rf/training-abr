f5d2230cda16bf1bf5a99e908c01298407fd5e14
ryo.nagata <ryo.nagata@chillnn.com>
2021-09-25 11:51:09 +0900

    update: abr

--- pull.sh
+++ pull.sh
@@ -19,7 +19,7 @@ fi
 if [ $branch != 'exit' ]; then
     echo ""
     echo ""
-    echo "\033[43;31m$branch ABR HOTELをFetchします。\033[0m"
+    echo "\033[43;31m$branch ABR TrainingをFetchします。\033[0m"
     yarn add git+ssh://git@github.com:ryoNagata001/chillnn-training-abr.git#${branch}
 fi
 
