1e0152c47d8b4ef7d5a08424b56d417e9dcb6fdc
ryo.nagata <ryo.nagata@chillnn.com>
2021-09-26 18:02:22 +0900

    udpate

--- dist/util/index.d.ts
+++ dist/util/index.d.ts
@@ -1,1 +1,2 @@
 export * from './modules/ChillnnTrainingError';
+export * from './modules/IDGenerator';
--- dist/util/index.js
+++ dist/util/index.js
@@ -11,3 +11,4 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./modules/ChillnnTrainingError"), exports);
+__exportStar(require("./modules/IDGenerator"), exports);
